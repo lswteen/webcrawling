bb2e72a19d212f5b4bc54cc2f03451385428d83b
이순우 <renzo.1980@jobkorea.co.kr>
2022-10-20 16:33:57 +0900

    [main] python기본 파싱 진행

---
+++ main.py
@@ -0,0 +1,35 @@
+# main.py
+import requests
+from fastapi import FastAPI # FastAPI 모듈 가져오기
+from bs4 import BeautifulSoup
+
+app = FastAPI() # 객체 생성
+
+@app.get("/") # Route Path
+def test_index():
+
+    # Json 타입으로 값 반환
+    return {
+	    "Python": "Framework",
+	}
+
+@app.get("/something")
+def something():
+    return {
+        "Something": "Page",
+    }
+
+@app.get("/weba")
+def web_a():
+    url = 'https://kin.naver.com/search/list.nhn?query=%ED%8C%8C%EC%9D%B4%EC%8D%AC'
+    response = requests.get(url)
+    answer = ''
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        print(soup)
+    else:
+        print(response.status_code)
+    return {
+        "json"
+    }
